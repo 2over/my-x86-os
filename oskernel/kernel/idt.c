50de0d34e0723ca1da8490d5e585fc7e3f01f40e
vinpink <mysunshine2126@gmail.com>
2025-06-16 01:46:27 -0700

    feat: 加入时钟中断

--- oskernel/kernel/idt.c
+++ oskernel/kernel/idt.c
@@ -9,9 +9,10 @@ interrupt_gate_t  interrupt_table[INTERRUPT_TABLE_SIZE] = {0};
 //char idt_ptr[6] = {0};
 
 xdt_ptr_t  idt_ptr;
-//extern void interrupt_handler();
+
 extern void keymap_handler_entry();
 extern void interrupt_handler_entry();
+extern void clock_handler_entry();
 
 // 是在汇编中定义的
 extern int interrupt_handler_table[0x2f];
@@ -29,6 +30,10 @@ void idt_init() {
         if (i <= 0x15) {
             handler = (int)interrupt_handler_table[i];
         }
+        if (0x20 == i) {
+            handler = (int)clock_handler_entry;
+        }
+
         if (0x21 == i) {
             handler = keymap_handler_entry;
         }
