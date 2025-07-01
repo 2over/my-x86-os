8cedcceada99eefd600b2467d95b76dcff15ec6e
vinpink <mysunshine2126@gmail.com>
2025-06-30 17:42:15 -0700

    feat: 获取网卡的基址寄存器的值

--- oskernel/kernel/kernel_thread.c
+++ oskernel/kernel/kernel_thread.c
@@ -1,6 +1,11 @@
 #include "../include/linux/kernel.h"
 
-void* kernel_thread(void* arg) {
+/**
+ * 获取网卡的base address 0
+ * 只获取第一块网卡的数据，暂时不支持多块网卡
+ */
+int get_network_card_ba0() {
+    int ba0 = 0;
     int bus_max = 255;
     int dev_max = 32;
     int func_max = 7;
@@ -15,21 +20,27 @@ void* kernel_thread(void* arg) {
                 int v = in_32(0xCFC);
 
                 // 返回结果不是01xffff, 就说明那个位置有设备存在
-                if (((v & 0xFFFF) != 0xFFFF) && (v != 0)) {
-                    printk("bus: %02d dev: %02d fun: %02d", bus, dev, fun);
-
-                    out_32(0xCF8, addr | (3 << 2));
-                    uint header_type = (in_32(0xCFC) & 0x00ff0000) >> 16;
+                if (v != 0 && (v & 0xFFFF) != 0xFFFF) {
 
                     out_32(0xCF8, addr | (2 << 2));
-                    unsigned int class_code = (in_32(0xCFC) & 0xff000000) >> 24;
 
-                    printk("vID: %04x  dID: %04x, header_type: %04x, class_code:%04x\n",
-                           (v & 0xFFFF), (v & 0xFFFF0000) >> 16, header_type, class_code);
+                    int class_code = (in_32(0xCFC) & 0xff000000) >> 24;
 
-                    printk("\n---------------------------------------------------------------\n");
+                    // class code为2表示是网卡设备
+                    if (2 == class_code) {
+                        out_32(0xCF8, addr | (4 << 2));
+                        ba0 = in_32(0xCFC);
+
+                        break;
+                    }
                 }
             }
         }
     }
+
+    return ba0;
+}
+void* kernel_thread(void* arg) {
+    int ba0 = get_network_card_ba0();
+    printk("%x\n", ba0);
 }
